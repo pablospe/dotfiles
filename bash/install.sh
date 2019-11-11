e30f344df5f633b53f84eecf6b42db2e5e8c9540
Pablo Speciale <pablo.speciale@gmail.com>
2019-11-11 11:56:35 +0100

    forgit (fzf for git)

--- bash/install.sh
+++ bash/install.sh
@@ -42,6 +42,15 @@ echo '# fzf settings' >>~/.bashrc
 echo 'source $HOME/.bash/fzf.sh' >>~/.bashrc
 
 #
+# forgit (fzf for git)
+#
+wget https://raw.githubusercontent.com/wfxr/forgit/master/forgit.plugin.zsh
+cp forgit.plugin.zsh $HOME/.bash/
+echo '' >>~/.bashrc
+echo '# forgit (fzf for git)' >>~/.bashrc
+echo 'source $HOME/.bash/forgit.plugin.sh' >> $HOME/.bashrc
+
+#
 # bash-git-prompt
 #
 rm -fr ~/.bash-git-prompt
