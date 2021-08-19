fd2a5d0e7cff12b9a4a2b6031402da147170e0a2
Pablo Speciale <pablo.speciale@gmail.com>
2021-08-19 21:48:13 +0200

    replace name: base16 -> .tmux.theme

--- install_tmux.sh
+++ install_tmux.sh
@@ -8,6 +8,6 @@ git clone --dept 1 https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 cp .tmux.conf $HOME/
 
-chmod +x base16.sh 
-cp base16.sh $HOME/
+chmod +x .tmux.theme
+cp .tmux.theme $HOME
 
